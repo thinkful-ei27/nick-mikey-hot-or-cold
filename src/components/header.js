8e338afb340e61ad818883ab825d0d899fbd1793
nickJ <nicknotjustin@gmail.com>
2019-01-22 14:48:27 -0600

    index.css given background-color. header.js made in new folder components.

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header(){
+    return (<header>
+      <nav>
+          <a className='What' href="">WHAT ?</a>
+          <a className='newGame' href="">New Game</a>
+      </nav>
+      <h1>HOT or COLD</h1>    
+    </header>);
+}
